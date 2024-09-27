0bb96a0e464102b297ba64392d6d8ffb1190a499
FlameDev <88896453+FlameOff@users.noreply.github.com>
2024-09-27 11:56:04 -0300

    Update trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -1,34 +1,36 @@
-1)
+#!/bin/bash
+
+# 1)
 sudo apt-get update
 sudo apt-get install fortune
 
-2)
+# 2)
 fortune > mensagem1.txt
 fortune > mensagem2.txt
 fortune > mensagem3.txt
 
-3)
+# 3)
 cat mensagem1.txt mensagem2.txt mensagem3.txt > mensagens.txt
 cat mensagens.txt
 
-4)
+# 4)
 cp mensagens.txt mensagens_antiga.txt
 fortune >> mensagens.txt
 
-5)
+# 5)
 wc -l < mensagens.txt > linhas.txt
 
-6)
+# 6)
 egrep -o "a" mensagens.txt | wc -l > letras.txt
 
-7)
+# 7)
 ls -R /etc > saida.txt 2> erros.txt
 
-8)
+# 8)
 ls -R /etc &> saida_com_erros.txt
 
-9)
+# 9)
 cut -d: f1 /etc/psswd > usuarios.txt
 
-10)
+# 10)
 cut -d: f1 /etc/passwd | sort > usuarios_ordenados.txt#!/bin/bash
